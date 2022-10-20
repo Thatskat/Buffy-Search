cba042951358314de12ccd2420669bbc8018a1bb
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-10-20 18:28:35 +1100

    finished color change js

---
+++ js/colorchange.js
@@ -0,0 +1,15 @@
+var granimInstance = new Granim({
+  element: '#Granim',
+  direction: 'left-right',
+  isPausedWhenNotInView: false,
+  states : {
+      "default-state": {
+          gradients: [
+              ['#334155', '#1e293b'],
+              ['#4b5563', '#1f2937'],
+              ['#52525b', '#27272a']
+          ]
+      }
+  }
+});
+
