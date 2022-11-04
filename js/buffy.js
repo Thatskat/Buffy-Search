fee371772c6a52c2d291b8d9a28a64f741d29e28
Thatskat <109633947+Thatskat@users.noreply.github.com>
2022-11-04 15:05:22 +1100

    Added documentation

--- js/buffy.js
+++ js/buffy.js
@@ -1,6 +1,6 @@
-const buffySeason = document.getElementById('buffyseason');
-const buffyEpisode = document.getElementById('buffyepisode');
-const buffySearchBtn = document.getElementById('buffybtn');
+const buffySeason = document.getElementById('buffyseason'); //Gets the season input from index.html
+const buffyEpisode = document.getElementById('buffyepisode'); //Gets the episode input from index.html
+const buffySearchBtn = document.getElementById('buffybtn'); //The search button from idex.html
 const buffySearchSection = document.getElementById('search-section')
 
 const errorMessage = document.getElementById('errormessage');
@@ -15,15 +15,32 @@ const epQuote = document.getElementById('ep-quote');
 const epTrivia = document.getElementById('ep-trivia');
 const imdbLink = document.getElementById('imdb-link');
 
+
+/**
+ * @const {string}
+ */
 const apiURL = "https://buffy-the-vampire-slayer-api.herokuapp.com/";
+/**
+ * @const {string}
+ */
 const apiEpisode = "episode/";
+/**
+ * @const {string}
+ */
 const apiSeason = "/season/";
 
+// Hides the error Messages and Search Results page 
 errorMessage.style.display = 'none';
 searchResults.style.display = 'none';
 
-buffySearchBtn.addEventListener('click', validateForm);
+buffySearchBtn.addEventListener('click', validateForm); //CLick event on button will start the search for the specific episode
 
+
+/**
+ * validateFrom() - validates the inputted values to see if it complies with how many episodes and season there are 
+ * 
+ * @returns {string} - If inputted values pass the validation, buffySeason and buffyEpisode values will be passed onto the search()
+ */
 
 function validateForm(){
   let seasonNumber = buffySeason.value;
@@ -39,7 +56,7 @@ function validateForm(){
     alert("the episode number MUST be filled out!");
     return false;
   }else if(seasonNumber == "1" && episodeNumber > 12){
-    alert("season one only has 12 episodes! pick an episode number between 1 and 12");
+    alert("season one only has 12 episodes!");
     return false;
   } else if(seasonNumber > 7){
     alert("there are only 7 seasons of buffy!");
@@ -49,11 +66,17 @@ function validateForm(){
     return false;
   }
   else {
-    Search()
+    search()
   }
 }
 
-function Search() {
+/**
+ * search() - receives the inputted buffySeason and buffyEpisode values and passes them as parameters for the findData()
+ * buffySeason and buffyEpisode values are then reset to ""
+ * 
+ * @return {Parameters}
+ */
+function search() {
   const seasonValue = buffySeason.value;
   const episodeValue = buffyEpisode.value;
   findData(seasonValue, episodeValue);
@@ -61,6 +84,15 @@ function Search() {
   buffyEpisode.value = "";
 }
 
+/**
+ * 
+ * @param {*} seasonValue 
+ * @param {*} episodeValue 
+ * 
+ * findData() - returns the episode data of the inputted Buffy episode
+ * 
+ * @return {object} - JSON object containing the information of the specified Buffy episode
+ */
 async function findData(seasonValue, episodeValue){
 try {
 const res = await fetch(`${apiURL}${apiEpisode}${episodeValue}${apiSeason}${seasonValue}`);
@@ -73,6 +105,15 @@ searchResults.style.display = 'none';
 }
 }
 
+/**
+ * 
+ * @param {*} jsonData 
+ * 
+ * displayData() - Displays the buffy json data in a presentable fashion
+ * json data included is episode title, season and episode number, director, original broadcast date, the episode's main plot, a quote from the episode, a piece of trivia as well as the link to that episode's imdb page for further information
+ * 
+ *
+ */
 function displayData(jsonData) {
   console.log(jsonData.error);
   if(jsonData.error) {
